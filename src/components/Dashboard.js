47f8a4bab70541e5c0211cceace12fe4ea3d0be6
Jared Flomen <jaredflomen@gmail.com>
2020-12-17 13:14:31 -0500

    Adding state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,9 +3,12 @@ import React, { Component } from "react";
 import classnames from "classnames";
 
 class Dashboard extends Component {
+  state = {
+    loading: true
+  }
+  
   render() {
     const dashboardClasses = classnames("dashboard");
-
     return <main className={dashboardClasses} />;
   }
 }
